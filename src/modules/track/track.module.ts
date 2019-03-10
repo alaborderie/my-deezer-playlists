6ff2ae0f9f7205a53cc9a00ebecba6259f53b055
Antoine Laborderie <alaborderie@lekooa.com>
2019-03-10 14:06:21 +0100

    Fixed dateFormat to add 0 if minutes < 10

--- src/modules/track/track.module.ts
+++ src/modules/track/track.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackListComponent } from './components/track-list/track-list.component';
 import {InfiniteScrollModule} from "ngx-infinite-scroll";
+import {UtilsModule} from "../utils/utils.module";
 
 @NgModule({
   declarations: [TrackListComponent],
   imports: [
     CommonModule,
-    InfiniteScrollModule
+    InfiniteScrollModule,
+    UtilsModule
   ],
   exports: [
     TrackListComponent
--- src/modules/utils/pipes/date-format.pipe.ts
+++ src/modules/utils/pipes/date-format.pipe.ts
@@ -6,12 +6,10 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class DateFormatPipe implements PipeTransform {
 
   transform(value: number, args?: any): any {
-    console.log(value);
     const hours: number = parseInt((value / 3600).toString(), 10);
     const minutes: number = parseInt(((value - (hours * 3600)) / 60).toString(), 10);
     const seconds: number = value - ((hours * 3600) + (minutes * 60));
-    console.log(`${hours}:${minutes}:${seconds}`);
-    return `${hours}:${minutes}:${seconds}`;
+    return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
   }
 
 }
